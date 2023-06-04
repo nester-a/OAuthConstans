92a215424fbf36aecf2fd39cae9857914392e7b9
Aleksey_N <nester.a.l@yandex.ru>
2023-06-04 16:39:19 +0300

    добавлены новые константы

---
+++ OAuthConstans/ProtocolEndpoint.cs
@@ -0,0 +1,9 @@
+﻿namespace OAuthConstans
+{
+    public static class ProtocolEndpoint
+    {
+        public const string Authorization = "/authorization";
+        public const string Token = "/token";
+        public const string Revoke = "/revoke";
+    }
+}
