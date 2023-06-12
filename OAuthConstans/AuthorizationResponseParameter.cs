e13c77f2c91661fdb0bc11f9bc075ab1fccb6561
Aleksey_N <nester.a.l@yandex.ru>
2023-06-12 21:32:28 +0300

    добавлена страница с авторизацией, данные начали передаваться

---
+++ OAuthConstans/AuthorizationResponseParameter.cs
@@ -0,0 +1,8 @@
+﻿namespace OAuthConstans
+{
+    public static class AuthorizationResponseParameter
+    {
+        public const string Code = "code";
+        public const string State = "state";
+    }
+}
